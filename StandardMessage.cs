fcb613029d54f2d865ebda0a29ba3c836c114e18
bond oji <ojibond150@gmail.com>
2022-06-24 10:22:38 -0500

    Standard UI message to prompt user and end application.

---
+++ StandardMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMunging
+{
+    public class StandardMessage
+    {
+        public static string WelcomeMesage()
+        {
+            Console.WriteLine("Welcome, enter weather or football for relevant data processing: ");
+            string userChoice = Console.ReadLine();
+            return userChoice;
+        }
+
+        public static void EndApplication()
+        {
+            Console.ReadLine();
+        }
+
+        public static void ExitApplication()
+        {
+            Console.WriteLine("Press any key to end the application: ");
+            Console.ReadLine();
+
+        }
+    }
+}
